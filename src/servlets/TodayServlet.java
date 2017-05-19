5a5a5ccc2d3694a5ba5bdaa5287b42026f7fe6c5
srikanthpragada <srikanthpragada@gmail.com>
2017-05-19 11:28:15 +0530

    19-MAY

--- src/servlets/TodayServlet.java
+++ src/servlets/TodayServlet.java
@@ -10,14 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/today")
+// @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
+	int count = 1; 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		 response.setContentType("text/html");
 		 PrintWriter pw = response.getWriter();
 		 pw.println("<h2>" + new Date().toString() + "</h2>");
+		 String title = getServletConfig().getInitParameter("title");
+		 pw.println("<hr/>");
+		 pw.println("<h4>" + title  + "</h4>");
+		 pw.println("<h3>" + count + "</h3");
+		 count ++;
 	}
 
 }
