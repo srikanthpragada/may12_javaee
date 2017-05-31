35dd8ed8dc09d8cfd5f648faf9dcf2b4f78a219e
srikanthpragada <srikanthpragada@gmail.com>
2017-05-31 09:58:19 +0530

    31-MAY

---
+++ src/jaxws/HelloService.java
@@ -0,0 +1,14 @@
+package jaxws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService 
+public class HelloService {
+	
+	 @WebMethod 
+	 public String hello(String name) {
+		 return  "Hello, " + name;
+	 }
+
+}
